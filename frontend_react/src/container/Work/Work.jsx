9e5677cf3bce5a1bd2ad1971e5d42ea33682a25d
Lindiwe Mutungamiri <lindiwemutungamiri23@gmail.com>
2022-09-15 15:34:42 +0000

    backend stuff

---
+++ frontend_react/src/container/Work/Work.jsx
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react';
+import {AiFillEye, AiFillGithub} from 'react-icons/ai';
+import {motion} from 'framer-motion';
+
+import {AppWrap} from '../../wrapper';
+import {uriFor, client, urlFor } from '../../client';
+
+import './Work.scss';
+
+const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState();
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query)
+      .then((data)=>{
+        setWorks(data);
+        setFilterWork(data);
+      })
+   
+  }, [])
+
+
+
+  const handleWorkFilter = (item) =>{
+
+  }
+  return (
+   <>
+    <h2 className='head-text'>My<span> Technology</span> & <span>Entrepreneurship</span> Portfolio</h2>
+    <div className='app__work-filter'>
+      {['Entrepreneurship', 'Web 3', 'Web 2', 'Research', 'All'].map((item, index) => (
+        <div
+        key={index}
+        onClick = {() => handleWorkFilter(item)}
+        className = {`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+        >
+          {item}
+        </div>
+
+      ))}
+
+    </div>
+    <motion.div
+    animate = {animateCard}
+    transition={{duration: 0.5, delayChildren: 0.5}}
+        className= 'app__work-portfolio'
+    >
+      {filterWork.map((work, index) =>(
+        <div className='app__work-item app__flex' key={index}>
+          <div className='app__work-img app__flex'>
+            <img src={urlFor(work.imgUrl)} alt={work.name} />
+            <motion.div
+            whileHover={{opacity: [0, 1]}}
+
+            >
+
+            </motion.div>
+          </div>
+        </div>
+
+      ))}
+
+    </motion.div>
+
+   </>
+  )
+}
+
+export default Work
