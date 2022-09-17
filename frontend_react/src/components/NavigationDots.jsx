a6273ba419f419dc6bae39855c336b10eb8e4f4d
Lindiwe Mutungamiri <lindiwemutungamiri23@gmail.com>
2022-09-17 17:46:50 +0000

    full frontend done

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
 
-const NavigationDots = ({active}) => {
-  return (
-    <div className='app__navigation'>
-          {['Home', 'About', 'Work', 'Skills','Testimonials', 'Contact'].map((item, index) => (
-                <a 
-                href= {`#${item}`}
-                key={item + index}
-                className = 'app__navigation-dot'
-                style={active === item?{backgroundColor: '#313BAC'} : {}  }
-                />    
-            ))}
+import React from 'react';
 
-    </div>
-  )
-}
+const NavigationDots = ({ active }) => (
+  <div className="app__navigation">
+    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+      <a
+        href={`#${item}`}
+        key={item + index}
+        className="app__navigation-dot"
+        style={active === item ? { backgroundColor: '#313BAC' } : {}}
+      />
+    ))}
+  </div>
+);
 
-export default NavigationDots
+export default NavigationDots;
--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM  from 'react-dom';
+//import React from 'react';
+//import ReactDOM  from 'react-dom';
 
+
+//import App from './App';
+//import './index.css';
+
+//ReactDOM.render(<App/>, document.getElementById('root'));
+
+import React from "react";
+import {createRoot} from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
